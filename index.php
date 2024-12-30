e0b0dc39d1d54e1a0a4ceeb0c21791b23df58cdf
Oscar Enrique Rivera Say <oscarrivera4901@gmail.com>
2024-12-29 21:24:24 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$name = "Miguel";
+?>
+<h1>
+<?= "Hello World!";?>
+</h1>
+
+<style>
+    :root {
+        color-scheme: light dark;
+        font-family: Arial, sans-serif;
+    }
+    body{
+        display: grid;
+        place-content: center;
+    }
+</style>
